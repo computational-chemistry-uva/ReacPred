6fda914735ebc2a1607b1e33b9c1ce4e703fd265
Alberto PérezdeAlba Ortíz <apdealbao@gmail.com>
2025-09-18 10:15:41 +0200

    Add files via upload

---
+++ create_magpie.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 15:14:33 2024
+
+@author: korotkevich
+"""
+
+import pandas as pd
+import json
+
+from pymatgen.core.composition import Composition
+from matminer.featurizers.composition import ElementProperty as ep
+
+path_data = 'data/Dm/dm_f_H.csv' #example path to the dataset you want tp featurize
+df = pd.read_csv(path_data)
+
+#Extract unique stoichiometries
+compositions = df['Base salt'].unique().tolist() #column with stoichiometries
+
+#Initialize featurizer
+featurizer = ep.from_preset("magpie")
+
+magpie_features = {}
+
+for salt in compositions:
+    
+    comp = Composition(salt)
+    magpie_feature = featurizer.featurize(comp)
+    magpie_features[salt] = magpie_feature
+
+
+    
+with open('magpie_features_file.json', 'w') as f:
+    json.dump(magpie_features, f)
+
+
+ 
